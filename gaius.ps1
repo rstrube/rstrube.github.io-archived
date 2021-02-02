330db459e7bef5da6522c014a8da294385aff756
Robert Strube <rstrube@gmail.com>
2021-02-02 16:07:56 -0700

    Upgraded to Gaius v0.0.53

--- gaius.ps1
+++ gaius.ps1
@@ -1,6 +1,6 @@
 # Gaius CLI wrapper for Windows
 
-$version = "0.0.52"
+$version = "0.0.53"
 
 function Main() {
 
--- gaius.sh
+++ gaius.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Gaius CLI wrapper for Linux/OSX
 
-VERSION="0.0.52"
+VERSION="0.0.53"
 
 function main() {
 
