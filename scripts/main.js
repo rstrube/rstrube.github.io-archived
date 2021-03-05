488d517783c8d77d5416f8e8e0d19c8a893cf92c
rstrube <rstrube@gmail.com>
2021-03-05 04:14:17 +0000

    Update gh-pages to output generated at fd50dd0

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('#sidebar-expanded-toggler').on('click', function () {
+        $('#sidebar').toggleClass('toggled');
+    });
+
+    $('#sidebar-collapsed-toggler').on('click', function () {
+        $('#sidebar').toggleClass('toggled');
+    });
+
+});
